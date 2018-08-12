3bd1808e3c3ca6bf0542c1866fa27b38befdee00
Richard Fenix <richard.fenix@gmail.com>
2018-08-12 03:41:06 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -49,6 +49,7 @@ class ApplicationController < Sinatra::Base
     else
       #User.create(username: params[:username], password: params[:password])
       user = User.find_by(username: params[:username])
+      binding.pry
       if user && user.authenticate(params[:password])
         session[:id] = user.id
         redirect '/account'
